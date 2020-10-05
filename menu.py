c83d090c81ac3ed70aeb59354002e2def37d057b
doppelsito <maur.barriga@alumnos.duoc.cl>
2020-10-05 10:02:16 -0300

    se agrego opcion personas

--- menu.py
+++ menu.py
@@ -16,7 +16,29 @@ def Personas():
     print("--- Opcion de Datos de Personas ---")
     #ingresar para n personas el nombre y la edad. n debe ser ingresado por teclado
     #mostrar: cuantas personas son mayores de edad y cuantas son menores de edad
+	def personas():
+    print("--- Opción de Datos de Personas ---")
+    #ingresar para n personas el nombre y la edad. n debe ser ingresado por teclado
+    #mostrar: cuantas personas son mayores de edad y cuantas son menores de edad.
+    #subir un tercer commit a git con el mensaje "se agrego la opcion dos al menú"
 
+    mayores = 0
+    menores = 0
+    x = 1
+    n=int (input("Cuantas personas desea ingresar?: "))
+    while (x<=n):
+        input("Nombre de la persona?: ")
+        edad=int (input("Que Edad tiene?: "))
+        if(edad <= 18):
+            mayores = mayores + 1
+        elif (edad >18):
+            menores = menores + 1
+        else:
+            break
+        x = x + 1
+    print("Cantidad de Mayores de edad: " + str(mayores))
+    print("Cantidad de Manosres de edad: " + str(menores))
+    tecla=input("Presione una tecla para continuar")
 
 seguir=True
 while seguir:
